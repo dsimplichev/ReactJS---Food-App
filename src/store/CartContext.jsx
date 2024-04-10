f1c31c96ba2edbc3c7963e5c8a4f8dd2bdf86f4e
dsimplichev <dsimplichev1948@gmail.com>
2024-04-10 21:55:12 +0100

    working on removing item from card

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -29,13 +29,28 @@ function cartReducer(state, action) {
     }
 
     if (action.type === 'REMOVE_ITEM') {
+        const existingCartItemIndex = state.items.findInex((item) => item.id === action.item.id)
+      
+        const existingCartItem = state.items[existingCartItemIndex]
 
+        if(existingCartItem === 1) {
+            const updatedItems = [...state.items];
+            updatedItems.splice(existingCartItemIndex, 1)
+        } else {
+            const updatedItem = { 
+                ...existingCartItem, 
+                quantity: existingCartItem.quantity - 1,
+            }
+            updatedItem[existingCartItemIndex] = updatedItem
+        }
+       
+        return {...state, items: updatedItems}
     }
 
     return state;
 }
 export function CartContextProvider({ children }) {
-    useReducer(cartReducer, { items: [] });
+  const [  ]  useReducer(cartReducer, { items: [] });
 
 
     return <CartContextProvider>{children}</CartContextProvider>
