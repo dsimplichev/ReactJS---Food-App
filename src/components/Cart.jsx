b25b0b575453830101ddaeaa0e0b54c79afd4bd2
dsimplichev <dsimplichev1948@gmail.com>
2024-04-11 15:49:58 +0100

    working on cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,16 @@
 import { useContext } from "react"
+
 import Modal from "./UI/Modal"
 import CartContext from "../store/CartContext"
-
+import { currencyFormatter } from '../util/formatting'
+import Button from "./UI/Button"
 
 export default function Cart() {
    const cartCtx = useContext(CartContext)
-   const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price )
+   const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0 )
 
     return (
-        <Modal className='cart'>
+        <Modal className='cart' open={true}>
             <h2>Your Cart</h2>
             <ul>
                {cartCtx.items.map((item) => (
@@ -17,7 +19,11 @@ export default function Cart() {
                 </li>
                 ))}
             </ul>
-            <p className="cart-total"></p>
+            <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+            <p className="modal-actions">
+              <Button textOnly>Close</Button>
+              <Button>Go to Checkout</Button>
+            </p>
         </Modal>
     )
 }
---
+++ src/store/userProgressContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+
+const UserProgressContext = createContext({
+    progress: '',
+    showCart: () => {},
+    hideCart: () => {},
+    showCheckout: () => {},
+    hideCheckout: () => {}
+ 
+})
+
+export function UserProgressContextProvider({children}) {
+    const [ userProgress, setUserProgress] = useState('')
+    
+   function showCart() {
+    setUserProgress('')
+   }
+
+   function showCheckout() {
+    setUserProgress('checkout')
+   }
+
+   function hideCheckout() {
+    setUserProgress('')
+   }
+
+    return (
+        <UserProgressContext.Provider>{children}</UserProgressContext.Provider>
+    )
+}
+
+export default UserProgressContext
