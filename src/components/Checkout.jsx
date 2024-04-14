3580d2e040ea6f6bd8828409830dda6b9062e51a
dsimplichev <dsimplichev1948@gmail.com>
2024-04-14 12:00:55 +0100

    some changes on checkout form

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -4,20 +4,34 @@ import Modal from "./UI/Modal"
 import CartContext from "../store/CartContext"
 import { currencyFormatter } from "../util/formatting"
 import Input from "./UI/Input"
+import UserProgressContext from "../store/userProgressContext"
 
 export default function Checkout() {
    const cartCtx = useContext(CartContext)
-   
+   const userProgressCtx = useContext(UserProgressContext)
    const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0)
    
+   function handleClose() {
+    userProgressCtx.hideCheckout()
+   }
    return (
-        <Modal>
+        <Modal open={userProgressCtx.progress === 'checkout'}>
             <form>
                 <h2>Checkoout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
             </form>
 
-            <Input label= "Full Name" type="text" />
+            <Input label="Full Name" type="text" />
+            <Input label="Email" type="email" id="email" />
+            <Input label="Street" type="text" id="street" />
+            <div className="control-row">
+                <Input label="Postal Code" type="text" id="postal-code" />
+                <Input label="City" type='text' id="city" />
+            </div>
+            <p className="modal-actions">
+                <Button type="button" textOnly onClick={handleClose}>Close</Button>
+                <Button>Submit Order</Button>
+            </p>
         </Modal>
     )
 }
